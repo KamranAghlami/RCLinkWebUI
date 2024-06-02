7b28c3c1b1a5aed8dbdfc418fb05b43ff1427deb
Kamran <oops.legendary@gmail.com>
2024-06-02 19:47:25 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './css/style.css';
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service_worker.js').then(registration => { }).catch(error => {
+        navigator.serviceWorker.register('service_worker.js').then(_ => { }).catch(error => {
             console.log('ServiceWorker registration failed: ', error);
         });
     });
